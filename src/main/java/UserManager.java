23f54848d17a91f43cf8051bbf5f22c1d9a22fb9
jaypark <soRjdla0709@gmail.com>
2024-12-16 14:20:25 +0900

    [로그인, 회원가입]

--- src/main/java/UserManager.java
+++ src/main/java/UserManager.java
@@ -6,50 +6,50 @@ public class UserManager {
 
     private static final String FILE_NAME = "login.txt";
 
-    public boolean signup(String id, String password) {
-        if (id.isEmpty() || password.isEmpty()) {
-            return false;
+    public boolean signup( String id, String password ) {
+        if ( id.isEmpty() || password.isEmpty() ) {
+            return false;x
         }
 
         if (isIdExists(id)) {
             return false;
         }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME, true))) {
+        try ( BufferedWriter writer = new BufferedWriter( new FileWriter( FILE_NAME, true ) )) {
             writer.write(id + "," + password);
             writer.newLine();
             return true;
-        } catch (IOException e) {
+        } catch ( IOException e ) {
             e.printStackTrace();
             return false;
         }
     }
 
-    public boolean login(String id, String password) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+    public boolean login( String id, String password ) {
+        try ( BufferedReader reader = new BufferedReader( new FileReader( FILE_NAME ) )) {
             String line;
-            while ((line = reader.readLine()) != null) {
-                String[] userInfo = line.split(",");
-                if (userInfo[0].equals(id) && userInfo[1].equals(password)) {
+            while ( ( line = reader.readLine() ) != null ) {
+                String[] userInfo = line.split("," );
+                if ( userInfo[ 0 ].equals( id ) && userInfo[ 1 ].equals( password ) ) {
                     return true;
                 }
             }
-        } catch (IOException e) {
+        } catch ( IOException e ) {
             e.printStackTrace();
         }
         return false;
     }
 
-    private boolean isIdExists(String id) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+    private boolean isIdExists( String id ) {
+        try ( BufferedReader reader = new BufferedReader( new FileReader( FILE_NAME ) )) {
             String line;
-            while ((line = reader.readLine()) != null) {
-                String[] userInfo = line.split(",");
-                if (userInfo[0].equals(id)) {
+            while ( ( line = reader.readLine() ) != null ) {
+                String[] userInfo = line.split("," );
+                if ( userInfo[ 0 ].equals( id ) ) {
                     return true;
                 }
             }
-        } catch (IOException e) {
+        } catch ( IOException e ) {
             e.printStackTrace();
         }
         return false;
