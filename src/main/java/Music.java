cedc0fcc5ad78edd8171e9a8771bab14dc390228
jaypark <soRjdla0709@gmail.com>
2024-12-16 13:48:52 +0900

    [음악 실행 및 저장]

---
+++ src/main/java/Music.java
@@ -0,0 +1,52 @@
+package main.java;
+
+import java.io.*;
+import javazoom.jl.player.Player;
+import java.io.File;
+
+public class Music extends Thread {
+    private Player player;
+    private boolean isLoop;
+    private File file;
+    private FileInputStream fis;
+    private BufferedInputStream bis;
+
+    public Music(String name, boolean isLoop) {
+        try {
+            this.isLoop = isLoop;
+            file = new File(getClass().getResource("../Resources/music/" + name).toURI());
+            fis = new FileInputStream(file);
+            bis = new BufferedInputStream(fis);
+            player = new Player(bis);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public int getTime() {
+        if ( player == null )
+            return 0;
+        return player.getPosition();
+    }
+
+    public void close() {
+        isLoop = false;
+        player.close();
+        this.interrupt();
+    }
+
+    @Override
+    public void run() {
+        try {
+            do {
+                player.play();
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                player = new Player(bis);
+            } while(isLoop);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+}
